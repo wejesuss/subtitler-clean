0a30dab2a1211b414a648f457d231588af7b88d4
Wemerson Jesus <wejesuss@gmail.com>
2021-12-03 14:56:25 -0300

    feat: ensure UploadController returns 400 if no valid language is provided

--- src/presentation/controllers/upload.ts
+++ src/presentation/controllers/upload.ts
@@ -1,3 +1,5 @@
+import { GetLanguages } from '../../utils/languages/getLanguages'
+
 export class UploadController {
   handle (httpRequest: any): any {
     const { body, file } = httpRequest
@@ -13,6 +15,15 @@ export class UploadController {
       return {
         statusCode: 400,
         body: new Error('No file information provided')
+      }
+    }
+
+    const getLanguages = new GetLanguages()
+    const nameOrError = getLanguages.verify(body.language)
+    if (nameOrError instanceof Error) {
+      return {
+        statusCode: 400,
+        body: new Error('No valid language provided')
       }
     }
   }
