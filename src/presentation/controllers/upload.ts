068f70c0cdacd503a2bc48c93606bfcb69e0f032
Wemerson Jesus <wejesuss@gmail.com>
2021-12-02 22:06:58 -0300

    feat: ensure UploadController returns an error message

--- src/presentation/controllers/upload.ts
+++ src/presentation/controllers/upload.ts
@@ -2,7 +2,8 @@ export class UploadController {
   handle (httpRequest: any): any {
     if (!httpRequest.body.language) {
       return {
-        statusCode: 400
+        statusCode: 400,
+        body: new Error('No language provided')
       }
     }
   }
