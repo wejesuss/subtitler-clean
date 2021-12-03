d0e43f1c2240a91633b0866da7d5d8c0ff2e770c
Wemerson Jesus <wejesuss@gmail.com>
2021-12-03 14:57:29 -0300

    test: ensure UploadController returns 400 if no valid language is provided

--- src/presentation/controllers/upload.spec.ts
+++ src/presentation/controllers/upload.spec.ts
@@ -13,6 +13,20 @@ describe('Upload Controller', () => {
     expect(httpResponse.body).toEqual(new Error('No language provided'))
   })
 
+  test('Should return 400 if no valid language is provided', () => {
+    const sut = new UploadController()
+    const httpRequest = {
+      body: {
+        language: 'dggd'
+      },
+      file: {}
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('No valid language provided'))
+  })
+
   test('Should return 400 if no file is provided', () => {
     const sut = new UploadController()
     const httpRequest = {
