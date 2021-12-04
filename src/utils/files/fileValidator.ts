b4320cce072830437d58f71808d297507a8440ab
Wemerson Jesus <wejesuss@gmail.com>
2021-12-04 14:08:56 -0300

    feat: ensure FileValidator returns an error if file size is too big

--- src/utils/files/fileValidator.ts
+++ src/utils/files/fileValidator.ts
@@ -9,7 +9,8 @@ interface File {
 
 export class FileValidator {
   private errorFields = ''
-  private readonly mimetypeRegEx = /^(audio|video)\/([0-9A-Za-z-_]+)$/g
+  private readonly mimetypeRegEx = /(audio|video)\/([0-9A-Za-z-_]+)/g
+  private readonly oneGigaByteLimit = 1073741824
 
   private verifyMimetype (mimetype: string): void {
     const isMimetypeValid = this.mimetypeRegEx.test(mimetype)
@@ -19,11 +20,20 @@ export class FileValidator {
     }
   }
 
+  private verifySize (size: number): void {
+    const limit = this.oneGigaByteLimit
+
+    if (size > limit) {
+      this.errorFields += this.errorFields ? ',size' : 'size'
+    }
+  }
+
   verify (fileInfo: File): any {
     this.errorFields = ''
-    const { mimetype } = fileInfo
+    const { mimetype, size } = fileInfo
 
     this.verifyMimetype(mimetype)
+    this.verifySize(size)
 
     if (this.errorFields) {
       const err = new Error('No valid file information provided')
