d0bd65435578d1cf7c2a2477781ec4afe016d0f3
Wemerson Jesus <wejesuss@gmail.com>
2021-12-04 16:29:56 -0300

    test: ensure UploadController returns 400 if no valid file information is provided

--- src/presentation/controllers/upload.spec.ts
+++ src/presentation/controllers/upload.spec.ts
@@ -37,4 +37,28 @@ describe('Upload Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('No file information provided'))
   })
+
+  test('Should return 400 if no valid file is provided', () => {
+    const sut = new UploadController()
+    const httpRequest = {
+      body: {
+        language: 'en'
+      },
+      file: {
+        mimetype: 'image/jpg',
+        size: (1073741824 + 1),
+        destination: 'public/uploads',
+        filename: 'input.jpg',
+        path: '/subtitler-clean/public/uploads/input.jpg'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    const err = new Error('No valid file information provided')
+    err.name = 'mimetype,size,buffer,destination,path'
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(err)
+    expect(httpResponse.body).toHaveProperty('name', err.name)
+  })
 })
