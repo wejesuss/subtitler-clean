b416df5e09c0372b51754501b2685697fe6a84a9
Wemerson Jesus <wejesuss@gmail.com>
2021-12-04 14:53:07 -0300

    test: ensure FileValidator returns an error if path does not exist and there is no Buffer

--- src/utils/files/fileValidator.spec.ts
+++ src/utils/files/fileValidator.spec.ts
@@ -30,7 +30,8 @@ describe('FileValidator', () => {
       size: greaterThan1GigaByte,
       destination: path.resolve(__dirname),
       filename: 'input.mp4',
-      path: path.resolve(__dirname, 'input.mp4')
+      path: path.resolve(__dirname, 'input.mp4'),
+      buffer: Buffer.from([23, 31, 124, 53])
     }
 
     const sut = new FileValidator()
@@ -57,7 +58,27 @@ describe('FileValidator', () => {
     const sut = new FileValidator()
     const fileOrError = sut.verify(file)
     const err = new Error('No valid file information provided')
-    err.name = 'mimetype,size'
+    err.name = 'mimetype,size,buffer'
+
+    expect(fileOrError).toEqual(err)
+    expect(fileOrError).toHaveProperty('name', err.name)
+  })
+
+  test('Should return an error if the path does not exist and there is no buffer', () => {
+    const uploadsFolder = path.resolve(__dirname, '../../../public/uploads')
+
+    const file = {
+      mimetype: 'audio/mpeg3',
+      size: 1073741824,
+      destination: uploadsFolder,
+      filename: 'input.mp3',
+      path: path.resolve(uploadsFolder, 'input.mp3')
+    }
+
+    const sut = new FileValidator()
+    const fileOrError = sut.verify(file)
+    const err = new Error('No valid file information provided')
+    err.name = 'buffer'
 
     expect(fileOrError).toEqual(err)
     expect(fileOrError).toHaveProperty('name', err.name)
