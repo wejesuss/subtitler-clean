0b7fb0d8524dc39adf80645bd35399bbfc60225d
Wemerson Jesus <wejesuss@gmail.com>
2021-12-08 14:52:50 -0300

    chore: remove default get coverage option from jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   clearMocks: true,
-  collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
