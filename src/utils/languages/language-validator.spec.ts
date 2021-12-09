663f09786cbdaf6427806cb71b141a9bd1b7ce5a
Wemerson Jesus <wejesuss@gmail.com>
2021-12-08 22:36:01 -0300

    refactor: make LanguageValidator conforms to a protocol

---
+++ src/utils/languages/language-validator.spec.ts
@@ -0,0 +1,46 @@
+import { LanguageValidator } from '../../presentation/protocols/language-validator'
+import { LanguageValidatorJsonRepo } from './language-validator'
+
+interface SutTypes {
+  sut: LanguageValidator
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new LanguageValidatorJsonRepo()
+
+  return {
+    sut
+  }
+}
+
+describe('Language Validator', () => {
+  test('Should return false if an invalid language is provided', () => {
+    const { sut } = makeSut()
+    jest.spyOn(sut, 'isValid').mockReturnValueOnce(false)
+
+    const language = 'invalid_language'
+    const isValid = sut.isValid(language)
+
+    expect(isValid).toBe(false)
+  })
+
+  test('Should return true if a valid language is provided', () => {
+    const { sut } = makeSut()
+    jest.spyOn(sut, 'isValid').mockReturnValue(true)
+
+    const language = 'valid_language'
+    const isValid = sut.isValid(language)
+
+    expect(isValid).toBe(true)
+  })
+
+  test('Should have been called with correct language', () => {
+    const { sut } = makeSut()
+    const isValidSpy = jest.spyOn(sut, 'isValid')
+
+    const language = 'valid_language'
+    sut.isValid(language)
+
+    expect(isValidSpy).toHaveBeenCalledWith(language)
+  })
+})
---
+++ src/utils/languages/language-validator.ts
@@ -0,0 +1,20 @@
+import { LanguageValidator } from '../../presentation/protocols/language-validator'
+import parsedLanguages from './languages.json'
+
+interface Languages {
+  [K: string]: string
+}
+
+export class LanguageValidatorJsonRepo implements LanguageValidator {
+  private readonly _languages: Languages
+
+  constructor () {
+    this._languages = parsedLanguages
+  }
+
+  isValid (language: string): boolean {
+    const found = Object.keys(this._languages).includes(language)
+
+    return found
+  }
+}
