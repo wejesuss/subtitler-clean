54988043de6b56c195ca048c67928c9484cfc3fb
Wemerson Jesus <wejesuss@gmail.com>
2021-12-10 12:05:44 -0300

    chore: update import/export strategy to make sure jest will collect correct coverage

--- src/presentation/controllers/upload/upload-protocols.ts
+++ src/presentation/controllers/upload/upload-protocols.ts
@@ -1,3 +1,3 @@
-export * from '../../protocols'
-export * from '../../protocols/file-validator'
-export * from '../../protocols/language-validator'
+export { Controller, HttpResponse, HttpRequest } from '../../protocols'
+export { File, FileValidator } from '../../protocols/file-validator'
+export { LanguageValidator } from '../../protocols/language-validator'
--- src/presentation/protocols/index.ts
+++ src/presentation/protocols/index.ts
@@ -1,2 +1,2 @@
-export * from './controller'
-export * from './http'
+export { HttpRequest, HttpResponse } from './http'
+export { Controller } from './controller'
