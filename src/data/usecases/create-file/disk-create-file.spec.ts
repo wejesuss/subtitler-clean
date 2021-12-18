bec4924219e2d5206133e4b22c0ab90aad5ed531
Wemerson Jesus <wejesuss@gmail.com>
2021-12-18 12:43:35 -0300

    refactor: move sut creation to a factory helper method

--- src/data/usecases/create-file/disk-create-file.spec.ts
+++ src/data/usecases/create-file/disk-create-file.spec.ts
@@ -1,15 +1,34 @@
 import { CreateFileStorage, CreateFileStorageModel } from './disk-create-file-protocols'
 import { DiskCreateFile } from './disk-create-file'
 
+const makeCreateFileStorage = (): CreateFileStorage => {
+  class CreateFileStorageStub implements CreateFileStorage {
+    async create (fileData: CreateFileStorageModel): Promise<boolean> {
+      return await Promise.resolve(true)
+    }
+  }
+
+  return new CreateFileStorageStub()
+}
+
+interface SutTypes {
+  sut: DiskCreateFile
+  createFileStorageStub: CreateFileStorage
+}
+
+const makeSut = (): SutTypes => {
+  const createFileStorageStub = makeCreateFileStorage()
+  const sut = new DiskCreateFile(createFileStorageStub)
+
+  return {
+    sut,
+    createFileStorageStub
+  }
+}
+
 describe('DiskCreateFile Usecase', () => {
   test('Should call CreateFileStorage with correct values', async () => {
-    class CreateFileStorageStub implements CreateFileStorage {
-      async create (fileData: CreateFileStorageModel): Promise<boolean> {
-        return await Promise.resolve(true)
-      }
-    }
-    const createFileStorageStub = new CreateFileStorageStub()
-    const sut = new DiskCreateFile(createFileStorageStub)
+    const { sut, createFileStorageStub } = makeSut()
     const createFileStorageSpy = jest.spyOn(createFileStorageStub, 'create')
 
     const fileData = {
