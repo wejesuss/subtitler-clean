5898476356e13ee3de96ebfb5bb827d9b0586e58
Wemerson Jesus <wejesuss@gmail.com>
2021-12-23 18:43:02 -0300

    chore: update jest coverage source

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   clearMocks: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
