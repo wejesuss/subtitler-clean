ef56ddcb4ba50612ca44bb9072e33c49235ebcee
Wemerson Jesus <wejesuss@gmail.com>
2021-12-28 22:10:26 -0300

    test: ensure SQLiteHelper reconnects if sqlite is down

--- src/infra/db/sqlite/helpers/sqlite-helper.spec.ts
+++ src/infra/db/sqlite/helpers/sqlite-helper.spec.ts
@@ -1,9 +1,63 @@
 import { SQLiteHelper as sut } from './sqlite-helper'
 
 describe('SQLiteHelper', () => {
+  beforeAll(async () => {
+    await sut.connect()
+  })
+
+  afterAll(async () => {
+    await sut.disconnect()
+  })
+
+  test('Should return false if connection is closed', async () => {
+    sut.client.close()
+
+    const promise = sut.isConnected()
+    await expect(promise).resolves.toBe(false)
+  })
+
+  test('Should reconnect if sqlite is down', async () => {
+    let files = await sut.getCollection('files')
+    expect(files).toBeTruthy()
+
+    await sut.disconnect()
+    files = await sut.getCollection('files')
+    expect(files).toBeTruthy()
+  })
+})
+
+describe('SQLiteHelper', () => {
   test('Should throw error if createDb throws', async () => {
     const promise = sut.createDb(':invalid:')
 
     await expect(promise).rejects.toThrow()
+    sut.client = null
+  })
+
+  test('Should call prepareDb reconnecting', async () => {
+    expect(sut.client).toBeNull()
+    const connectSpy = jest.spyOn(sut, 'connect')
+    await sut.prepareDb()
+
+    expect(connectSpy).toHaveBeenCalled()
+    await sut.disconnect()
+  })
+
+  test('Should call deleteAll reconnecting', async () => {
+    expect(sut.client).toBeNull()
+    const connectSpy = jest.spyOn(sut, 'connect')
+    await sut.deleteAll('files')
+
+    expect(connectSpy).toHaveBeenCalled()
+    await sut.disconnect()
+  })
+
+  test('Should call insertOne reconnecting', async () => {
+    expect(sut.client).toBeNull()
+    const connectSpy = jest.spyOn(sut, 'connect')
+    await sut.insertOne('files', {})
+
+    expect(connectSpy).toHaveBeenCalled()
+    await sut.disconnect()
   })
 })
