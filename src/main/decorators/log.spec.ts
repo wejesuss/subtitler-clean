4b939a0dfb060491a61104b3e70d6eb199477e64
Wemerson Jesus <wejesuss@gmail.com>
2021-12-31 13:12:41 -0300

    test: ensure LogControllerDecorator returns the same response as controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,19 +1,29 @@
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
 
+const makeFakeHttpRequest = (): HttpRequest => ({
+  body: { language: 'any_language' }
+})
+
+const makeFakeHttpResponse = (): HttpResponse => ({
+  statusCode: 200,
+  body: {
+    id: 'valid_id',
+    filename: 'valid_filename',
+    path: 'valid_path',
+    size: 1073741824
+  }
+})
+
 const makeControllerStub = (): Controller => {
   class ControllerStub implements Controller {
     async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-      return await Promise.resolve(null)
+      return await Promise.resolve(makeFakeHttpResponse())
     }
   }
 
   return new ControllerStub()
 }
-
-const makeFakeHttpRequest = (): HttpRequest => ({
-  body: { language: 'any_language' }
-})
 
 interface SutTypes {
   sut: LogControllerDecorator
@@ -37,5 +47,12 @@ describe('LogControllerDecorator', () => {
 
     await sut.handle(makeFakeHttpRequest())
     expect(controllerSpy).toHaveBeenCalledWith(makeFakeHttpRequest())
+  })
+
+  test('Should return the same httpResponse as controller', async () => {
+    const { sut } = makeSut()
+
+    const httpResponse = await sut.handle(makeFakeHttpRequest())
+    expect(httpResponse).toEqual(makeFakeHttpResponse())
   })
 })
