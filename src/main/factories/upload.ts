42537b27589850c6aa4cc5d92b576c6a7cb4513a
Wemerson Jesus <wejesuss@gmail.com>
2021-12-31 19:03:33 -0300

    feat: add LogSQLiteRepository to UploadFactory

--- src/main/factories/upload.ts
+++ src/main/factories/upload.ts
@@ -5,19 +5,19 @@ import { DiskCreateFile } from '../../data/usecases/create-file/disk-create-file
 import { DbAddFile } from '../../data/usecases/add-file/db-add-file'
 import { CreateFileLocalStorage } from '../../infra/disk/local/file-storage/file'
 import { FileSQLiteRepository } from '../../infra/db/sqlite/file-repository/file'
+import { LogSQLiteRepository } from '../../infra/db/sqlite/log-repository/log'
+import { LogControllerDecorator } from '../decorators/log'
+import { Controller } from '../../presentation/protocols'
 
-export const makeUploadController = (): UploadController => {
+export const makeUploadController = (): Controller => {
   const languageValidatorAdapter = new LanguageValidatorAdapter()
   const fileValidatorAdapter = new FileValidatorAdapter()
   const createFileLocalStorage = new CreateFileLocalStorage()
   const fileSQLiteRepository = new FileSQLiteRepository()
   const createFile = new DiskCreateFile(createFileLocalStorage)
   const addFile = new DbAddFile(fileSQLiteRepository)
+  const uploadController = new UploadController(languageValidatorAdapter, fileValidatorAdapter, createFile, addFile)
+  const logSQLiteRepository = new LogSQLiteRepository()
 
-  return new UploadController(
-    languageValidatorAdapter,
-    fileValidatorAdapter,
-    createFile,
-    addFile
-  )
+  return new LogControllerDecorator(uploadController, logSQLiteRepository)
 }
