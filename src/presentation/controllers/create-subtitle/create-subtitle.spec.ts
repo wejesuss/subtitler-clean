2e66b50fdb8632cc695d114caeaf529116484836
Wemerson Jesus <wejesuss@gmail.com>
2022-01-06 22:46:28 -0300

    test: ensure CreateSubtitleController calls GetFile with correct value

--- src/presentation/controllers/create-subtitle/create-subtitle.spec.ts
+++ src/presentation/controllers/create-subtitle/create-subtitle.spec.ts
@@ -1,16 +1,45 @@
 import { MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helper'
+import { HttpRequest } from '../../protocols'
+import { FileModel } from '../../../domain/models/file'
+import { GetFile } from '../../../domain/usecases/get-file'
 import { CreateSubtitleController } from './create-subtitle'
+
+const makeFakeHttpRequest = (): HttpRequest => ({
+  body: {
+    id: 'any_id'
+  }
+})
+
+const makeFakeFileModel = (): FileModel => ({
+  id: 'valid_id',
+  filename: 'valid_filename',
+  path: 'valid_path',
+  size: 1073741824
+})
+
+const makeGetFile = (): GetFile => {
+  class GetFileStub implements GetFile {
+    async get (id: string): Promise<FileModel> {
+      return await new Promise((resolve) => resolve(makeFakeFileModel()))
+    }
+  }
+
+  return new GetFileStub()
+}
 
 interface SutTypes {
   sut: CreateSubtitleController
+  getFileStub: GetFile
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new CreateSubtitleController()
+  const getFileStub = makeGetFile()
+  const sut = new CreateSubtitleController(getFileStub)
 
   return {
-    sut
+    sut,
+    getFileStub
   }
 }
 
@@ -25,5 +54,14 @@ describe('Create Subtitle Controller', () => {
     const httpResponse = await sut.handle(httpRequest)
 
     expect(httpResponse).toEqual(badRequest(new MissingParamError('id')))
+  })
+
+  test('Should call GetFile with correct value', async () => {
+    const { sut, getFileStub } = makeSut()
+    const getFileSpy = jest.spyOn(getFileStub, 'get')
+
+    await sut.handle(makeFakeHttpRequest())
+
+    expect(getFileSpy).toHaveBeenCalledWith('any_id')
   })
 })
