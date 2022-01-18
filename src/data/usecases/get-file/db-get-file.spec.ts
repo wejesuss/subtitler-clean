a46c6e41ec2066d5178931b6e8e3af054d5fef55
Wemerson Jesus <wejesuss@gmail.com>
2022-01-18 17:48:19 -0300

    test: ensure DbGetFile calls GetFileRepository with correct value

---
+++ src/data/usecases/get-file/db-get-file.spec.ts
@@ -0,0 +1,48 @@
+import { FileModel, GetFileRepository } from './db-get-file-protocols'
+import { DbGetFile } from './db-get-file'
+
+const makeFakeFile = (): FileModel => ({
+  id: 'valid_id',
+  mimetype: 'valid_mimetype',
+  language: 'valid_language',
+  filename: 'valid_filename',
+  path: 'valid_path',
+  size: 1073741824
+})
+
+const makeGetFileRepository = (): GetFileRepository => {
+  class GetFileRepositoryStub implements GetFileRepository {
+    async get (id: string): Promise<FileModel> {
+      return await new Promise((resolve) => resolve(makeFakeFile()))
+    }
+  }
+
+  return new GetFileRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbGetFile
+  getFileRepositoryStub: GetFileRepository
+}
+
+const makeSut = (): SutTypes => {
+  const getFileRepositoryStub = makeGetFileRepository()
+  const sut = new DbGetFile(getFileRepositoryStub)
+
+  return {
+    sut,
+    getFileRepositoryStub
+  }
+}
+
+describe('DbGetFile Usecase', () => {
+  test('Should call GetFileRepository with correct value', async () => {
+    const { sut, getFileRepositoryStub } = makeSut()
+    const getFileSpy = jest.spyOn(getFileRepositoryStub, 'get')
+    const id = 'valid_id'
+
+    await sut.get(id)
+
+    expect(getFileSpy).toHaveBeenCalledWith(id)
+  })
+})
