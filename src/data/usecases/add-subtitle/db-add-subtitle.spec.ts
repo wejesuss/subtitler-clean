8496efe3d2f26b97a71dd5ed92837b4cbc9d6ddf
Wemerson Jesus <wejesuss@gmail.com>
2022-01-18 22:28:47 -0300

    test: ensure DbAddSubtitle calls AddSubtitleRepository with correct values

---
+++ src/data/usecases/add-subtitle/db-add-subtitle.spec.ts
@@ -0,0 +1,56 @@
+import { AddSubtitleModel, AddSubtitleRepository, SubtitleModel } from './db-add-subtitle-protocols'
+import { DbAddSubtitle } from './db-add-subtitle'
+
+const makeFakeSubtitle = (): SubtitleModel => ({
+  id: 'valid_id',
+  language: 'valid_language',
+  sent_to_creation: true,
+  external_id: 'valid_external_id',
+  file_id: 'valid_file_id'
+})
+
+const makeValidFileData = (): AddSubtitleModel => ({
+  id: 'valid_id',
+  filename: 'valid_filename',
+  mimetype: 'valid_mimetype',
+  language: 'valid_language',
+  path: 'valid_path',
+  size: 1073741824
+})
+
+const makeAddSubtitleRepository = (): AddSubtitleRepository => {
+  class AddSubtitleRepositoryStub implements AddSubtitleRepository {
+    async add (file: AddSubtitleModel): Promise<SubtitleModel> {
+      return await new Promise((resolve) => resolve(makeFakeSubtitle()))
+    }
+  }
+
+  return new AddSubtitleRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbAddSubtitle
+  addSubtitleRepositoryStub: AddSubtitleRepository
+}
+
+const makeSut = (): SutTypes => {
+  const addSubtitleRepositoryStub = makeAddSubtitleRepository()
+  const sut = new DbAddSubtitle(addSubtitleRepositoryStub)
+
+  return {
+    sut,
+    addSubtitleRepositoryStub
+  }
+}
+
+describe('DbAddSubtitle Usecase', () => {
+  test('Should call AddSubtitleRepository with correct values', async () => {
+    const { sut, addSubtitleRepositoryStub } = makeSut()
+    const addSubtitleSpy = jest.spyOn(addSubtitleRepositoryStub, 'add')
+
+    const fileData = makeValidFileData()
+    await sut.add(fileData)
+
+    expect(addSubtitleSpy).toHaveBeenCalledWith(fileData)
+  })
+})
