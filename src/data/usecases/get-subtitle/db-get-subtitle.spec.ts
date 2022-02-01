e7995df299b3a8231e5dff5be7b288ce954d9c78
Wemerson Jesus <wejesuss@gmail.com>
2022-02-01 10:08:15 -0300

    test: ensure DbGetSubtitle calls GetSubtitleRepository with correct values

---
+++ src/data/usecases/get-subtitle/db-get-subtitle.spec.ts
@@ -0,0 +1,48 @@
+
+import { SubtitleModel, GetSubtitleRepository } from './db-get-subtitle-protocols'
+import { DbGetSubtitle } from './db-get-subtitle'
+
+const makeFakeSubtitle = (): SubtitleModel => ({
+  id: 'valid_id',
+  language: 'valid_language',
+  external_id: 'valid_external_id',
+  sent_to_creation: true,
+  file_id: 'valid_file_id'
+})
+
+const makeGetSubtitleRepository = (): GetSubtitleRepository => {
+  class GetSubtitleRepositoryStub implements GetSubtitleRepository {
+    async get (fileId: string): Promise<SubtitleModel> {
+      return await new Promise((resolve) => resolve(makeFakeSubtitle()))
+    }
+  }
+
+  return new GetSubtitleRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbGetSubtitle
+  getSubtitleRepositoryStub: GetSubtitleRepository
+}
+
+const makeSut = (): SutTypes => {
+  const getSubtitleRepositoryStub = makeGetSubtitleRepository()
+  const sut = new DbGetSubtitle(getSubtitleRepositoryStub)
+
+  return {
+    sut,
+    getSubtitleRepositoryStub
+  }
+}
+
+describe('DbGetSubtitle Usecase', () => {
+  test('Should call GetSubtitleRepository with correct value', async () => {
+    const { sut, getSubtitleRepositoryStub } = makeSut()
+    const getSubtitleSpy = jest.spyOn(getSubtitleRepositoryStub, 'get')
+    const id = 'valid_id'
+
+    await sut.get(id)
+
+    expect(getSubtitleSpy).toHaveBeenCalledWith(id)
+  })
+})
