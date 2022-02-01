f81601deceecf32a564cc84ef50323d32963cd48
Wemerson Jesus <wejesuss@gmail.com>
2022-02-01 11:41:15 -0300

    feat: ensure SubtitleSQLiteRepository calls getOneWhere with correct values

--- src/infra/db/sqlite/subtitle-repository/subtitle.ts
+++ src/infra/db/sqlite/subtitle-repository/subtitle.ts
@@ -1,9 +1,10 @@
 import { AddSubtitleRepository } from '../../../../data/protocols/add-subtitle-repository'
+import { GetSubtitleRepository } from '../../../../data/protocols/get-subtitle-repository'
 import { AddSubtitleModel } from '../../../../domain/usecases/add-subtitle'
 import { SubtitleModel } from '../../../../domain/models/subtitle'
 import { SQLiteHelper } from '../helpers/sqlite-helper'
 
-export class SubtitleSQLiteRepository implements AddSubtitleRepository {
+export class SubtitleSQLiteRepository implements AddSubtitleRepository, GetSubtitleRepository {
   async add (file: AddSubtitleModel): Promise<SubtitleModel> {
     const subtitleData = {
       language: file.language,
@@ -15,5 +16,11 @@ export class SubtitleSQLiteRepository implements AddSubtitleRepository {
     const subtitle = await SQLiteHelper.insertOne('subtitles', subtitleData)
 
     return subtitle
+  }
+
+  async get (fileId: string): Promise<SubtitleModel> {
+    await SQLiteHelper.getOneWhere('subtitles', { fieldName: 'file_id', id: fileId })
+
+    return await new Promise((resolve) => resolve(null))
   }
 }
