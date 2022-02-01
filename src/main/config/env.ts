757513a46cfe3b8e82c70569a5487022facd427a
Wemerson Jesus <wejesuss@gmail.com>
2022-02-01 16:34:11 -0300

    fix: add missing input image

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -4,7 +4,7 @@ import { web } from '../../../subtitler-credentials.json'
 export default {
   dbFilename: process.env.SQLITE_FILENAME || 'subtitler.db',
   port: process.env.PORT || 3000,
-  inputImage: process.env.INPUT_IMAGE || path.resolve(__dirname, '../../../public/image.jpg'),
+  inputImage: process.env.INPUT_IMAGE || path.resolve(__dirname, '../../../public/input.jpg'),
   clientId: web.client_id,
   clientSecret: web.client_secret,
   redirectUri: web.redirect_uris[0]
