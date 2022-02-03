233aeb4b4ff54ad4ac76f7553ea0df736d66f1b1
Wemerson Jesus <wejesuss@gmail.com>
2022-02-03 00:40:45 -0300

    test: ensure DownloadSubtitleController calls GetSubtitle with correct value

---
+++ src/presentation/controllers/download-subtitle/download-subtitle.spec.ts
@@ -0,0 +1,51 @@
+import { DownloadSubtitleController } from './download-subtitle'
+import { GetSubtitle } from '../../../domain/usecases/get-subtitle'
+import { SubtitleModel } from '../../../domain/models/subtitle'
+
+const makeFakeSubtitleModel = (): SubtitleModel => ({
+  id: 'any_id',
+  language: 'any_language',
+  sent_to_creation: true,
+  file_id: 'any_file_id',
+  external_id: 'any_external_id'
+})
+
+const makeGetSubtitle = (): GetSubtitle => {
+  class GetSubtitleStub implements GetSubtitle {
+    async get (fileId: string): Promise<SubtitleModel> {
+      return await new Promise((resolve) => resolve(makeFakeSubtitleModel()))
+    }
+  }
+
+  return new GetSubtitleStub()
+}
+
+interface SutTypes {
+  sut: DownloadSubtitleController
+  getSubtitleStub: GetSubtitle
+}
+
+const makeSut = (): SutTypes => {
+  const getSubtitleStub = makeGetSubtitle()
+  const sut = new DownloadSubtitleController(getSubtitleStub)
+
+  return {
+    sut,
+    getSubtitleStub
+  }
+}
+
+describe('Download Subtitle Controller', () => {
+  test('Should call GetSubtitle with correct value', async () => {
+    const { sut, getSubtitleStub } = makeSut()
+    const getSubtitleSpy = jest.spyOn(getSubtitleStub, 'get')
+
+    await sut.handle({
+      body: {
+        id: 'any_file_id'
+      }
+    })
+
+    expect(getSubtitleSpy).toHaveBeenCalledWith('any_file_id')
+  })
+})
