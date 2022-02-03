d4219526acbeceacbf67e5996474a79d5289b15c
Wemerson Jesus <wejesuss@gmail.com>
2022-02-03 11:54:26 -0300

    feat: ensure DownloadSubtitleController returns 400 if id is not provided

--- src/presentation/controllers/download-subtitle/download-subtitle.ts
+++ src/presentation/controllers/download-subtitle/download-subtitle.ts
@@ -1,6 +1,6 @@
-import { NotFoundError } from '../../errors'
-import { internalServerError, notFound } from '../../helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse, GetSubtitle } from './download-subtitle-protocols'
+import { MissingParamError, NotFoundError } from '../../errors'
+import { badRequest, internalServerError, notFound } from '../../helpers/http-helper'
 
 export class DownloadSubtitleController implements Controller {
   private readonly getSubtitle: GetSubtitle
@@ -12,6 +12,10 @@ export class DownloadSubtitleController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { id } = httpRequest.body
+      if (!id) {
+        return badRequest(new MissingParamError('id'))
+      }
+
       await this.getSubtitle.get(id)
       return notFound(new NotFoundError('subtitle'))
     } catch (error) {
