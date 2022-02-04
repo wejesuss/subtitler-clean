a13b69f2db557201fc910a778935ffedede65ada
Wemerson Jesus <wejesuss@gmail.com>
2022-02-03 21:28:04 -0300

    feat: add download-subtitle route

--- src/main/routes/subtitle/subtitle-routes.ts
+++ src/main/routes/subtitle/subtitle-routes.ts
@@ -3,6 +3,7 @@ import { Auth } from 'googleapis'
 import env from '../../config/env'
 import { adaptRoute } from '../../adapters/express-route-adapter'
 import { makeCreateSubtitleController } from '../../factories/create-subtitle'
+import { makeDownloadSubtitleController } from '../../factories/download-subtitle'
 import { OAuth2Client } from 'google-auth-library'
 
 const inputImage = env.inputImage
@@ -53,4 +54,5 @@ function getConsent (req: Request, res: Response): any {
 export default (router: Router): void => {
   router.get('/subtitle/credentials', getConsent)
   router.post('/create-subtitle', adaptRoute(makeCreateSubtitleController(inputImage, OAuthClient)))
+  router.get('/download-subtitle', adaptRoute(makeDownloadSubtitleController(inputImage, OAuthClient)))
 }
